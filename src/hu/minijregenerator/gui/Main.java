3edb71461f70d3dc2e425514bc9b3e4bd54ad05b
vpeter93 <vpeter93@hotmail.com>
2019-03-16 14:31:12 +0100

    create console version of the program

--- src/hu/minijregenerator/gui/Main.java
+++ src/hu/minijregenerator/gui/Main.java
@@ -13,13 +13,53 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		String classPath = "\"E:\\java\\KÉSZ programok\\CatDragonAdventures\\catDragon_lib/*\"";
-		String jarPath = "\"E:\\java\\KÉSZ programok\\CatDragonAdventures\\catDragon.jar\"";
-		String generatedJrePath = "minijre";
-		
-		MiniJreGenerator generator = new MiniJreGenerator();
-		generator.generate(classPath, jarPath, generatedJrePath);
+		if(args.length == 0)
+		{
 
+		}
+		if(args.length == 1)
+		{
+			if(args[0].equals("--help") || args[1].equals("-h"))
+				printUsage();
+		}
+		if(args.length == 6)
+			console(args);
+	}
+	public static void printUsage()
+	{
+		System.out.println("Usage : java -jar MiniJreGenerator.jar --class-path <path> --jar-path <path> --output-path <path>" );
+		System.out.println("OR java -jar MiniJreGenerator.jar -cp <path> -jp <path> -op <path>" );
+	}
+	public static void console(String[] args)
+	{
+		MiniJreGenerator generator = new MiniJreGenerator();
+		String classPath = null;
+		String jarPath = null;
+		String outputJrePath = null;
+		
+		for(int i = 0; i < args.length; i++)
+		{
+			if(args[i].equals("--class-path") || args[i].equals("-cp"))
+			{
+				classPath = args[i+1];
+			}
+			if(args[i].equals("--jar-path") || args[i].equals("-jp"))
+			{
+				jarPath = args[i+1];
+			}
+			if(args[i].equals("--output-path") || args[i].equals("-op"))
+			{
+				outputJrePath = args[i+1];
+			}
+		}
+		
+		if(classPath == null || jarPath == null || outputJrePath == null)
+		{
+			printUsage();
+			System.exit(-1);
+		}
+		
+		generator.generate(classPath, jarPath, outputJrePath);
 	}
 
 }
